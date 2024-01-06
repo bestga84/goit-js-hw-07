be49829f2b77be45f3bb2d0d8990b0da91b90c25
bestga84 <bestga84@gmail.com>
2024-01-06 21:33:25 +0200

    html and js
    1) До файлу 02-lightbox.html підключено стилі SimpleLightbox.
    2) В файлі 02-lightbox.js створено js.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// тут створення галереї 
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+);
+galleryList.insertAdjacentHTML("beforeend", markup.join(""));
+
+// тут SimpleLightbox 
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+
+  // Розмітка картки за таким шаблоном
+/*
+<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>
+*/
+
+
+// Фрагмент галереї
+// {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
